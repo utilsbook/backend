36fcac05e7f8c1b34c52a5d5073fa3a0b2d70cac
bertonyao <bertonyao@tencent.com>
2022-04-23 22:30:05 +0800

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const koa = require('koa');
+const https = require('https');
+const fs = require('fs');
+
+const app = new koa();
+
+// index page
+app.use(function* (next) {
+  this.body = 'hello world from ' + this.request.url;
+});
+
+// SSL options
+const options = {
+  key: fs.readFileSync('../ssl/icode.key'), //ssl文件路径
+  cert: fs.readFileSync('../ssl/icode.pem'), //ssl文件路径
+};
+
+https.createServer(options, app.callback()).listen(996);
+
+console.log('https server is running');
